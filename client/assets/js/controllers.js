ad66a86ed364c89cf5634323d959f2ea9cbe4290
akonsand <naved.merchant@gmail.com>
2016-01-19 19:59:08 -0500

    Implemented a brand new communcations service that will serve as the central point of communication between the Web client and the phone.
    Fixed a bug in the UI that made the UI explode when the text content overflowed

--- client/assets/js/controllers.js
+++ client/assets/js/controllers.js
@@ -1,41 +1,61 @@
 /**
  * Created by naved on 20/11/15.
  */
-var main =  angular.module('application');
+var main = angular.module('application');
 
-main.controller('LoginController',['$rootScope','$scope','localStorageService','peer',function($rootScope,$scope,localStorageService,peer){
-  if(typeof(peer) === "string"){
-    $scope.peerId = peer;
-    var thisPeer = new Peer(peer,{key:'ytgklpf684u0udi'});
-    $rootScope.thisPeer = thisPeer;
-  }
-  else {
-    $rootScope.$on('open', function (sender,peer) {
-      generateQRCode(peer.id);
-      $rootScope.thisPeer = peer;
-    })
-  }
-  function generateQRCode(peerId){
-    $scope.$apply(function(){
-      $scope.peerId = peerId;
-    });
-  }
+main.controller('LoginController',
+  ['$rootScope',
+    '$scope',
+    'localStorageService',
+    'communications',
+    '$state',
+    function ($rootScope,
+              $scope,
+              localStorageService,
+              communications,
+              $state) {
 
-  $rootScope.thisPeer.on('connection',function(conn){
-    conn.on('open',function(){
-      $rootScope.connected = true;
-      $rootScope.dataConnection = conn;
-    });
-  });
+      $rootScope.connectionOpened = false;
 
-  $rootScope.$on('$stateChangeSuccess',
-    function(event, toState, toParams, fromState, fromParams){
-      console.log('state change sucess');
-    });
 
-  $rootScope.$on('$stateChangeError',
-    function(event, toState, toParams, fromState, fromParams, error){
-      console.log('state change error');
-    });
+      var id = communications.getId($scope,function(id){
+        //in cases where the digest cycle is done, we call evalasync so that the scope is updated properly
+        $scope.$evalAsync(function(){
+          $scope.thisId = id;
+        });
+      });
 
-}]);
+      var otherPeerId = communications.getOtherId();
+      if(otherPeerId){
+        $state.go('chat');
+      }
+
+
+      //generateQRCode(id);
+
+      /**
+       * called internally for setting the peerId
+       * @param {string}id: The Id of the current peer
+       */
+      function generateQRCode(id) {
+        $scope.$apply(function (id) {
+          $scope.thisId = id;
+        });
+      }
+
+      communications.subscribeConnectionOpen($scope, function () {
+        $rootScope.connectionOpened = true;
+      });
+
+
+      $rootScope.$on('$stateChangeSuccess',
+        function (event, toState, toParams, fromState, fromParams) {
+          console.log('state change sucess');
+        });
+
+      $rootScope.$on('$stateChangeError',
+        function (event, toState, toParams, fromState, fromParams, error) {
+          console.log('state change error');
+        });
+
+    }]);
--- client/assets/js/services.js
+++ client/assets/js/services.js
@@ -1,20 +1,106 @@
 /**
  * Created by naved on 20/11/15.
  */
-var main =  angular.module('application');
+var main = angular.module('application');
 
-main.factory('peer',function(localStorageService,$rootScope){
-  var peerId = localStorageService.get('peerId');
-  if(!peerId){
-    var peer = new Peer({key:'ytgklpf684u0udi'});
-    peer.on('open',function(id){
-      peerId = id;
-      localStorageService.set('peerId',peerId);
-      $rootScope.$emit('open',peer);
-    });
-    return peerId;
-  }
-  else{
-    return peerId;
-  }
+
+main.factory('communications', function (localStorageService, $rootScope) {
+
+  var Communications = function () {
+    //try to get the peer id from local storage(if weve already had one before)
+    var id = localStorageService.get('peerId');
+    //try to get the remote peer id;
+    var otherPeerId = localStorageService.get('otherPeerId');
+    // by convention
+    var that = this;
+    //local object for holding the peer instance. Should not be accessed directly from the methods.
+    // use the getObject method to get the instance of the peer.
+    var peer;
+    //Empty connection object which will be filled later when the connection is established
+    this.connection = {};
+
+    /**
+     * This function is called when constructing the object, so that if the peer is not registered, it is registered
+     * with the server. It is only called once, after that the peer Id is saved.
+     */
+    var intialize = function () {
+      if (!id) {
+        peer = new Peer({key: 'ytgklpf684u0udi'});
+        peer.on('open', function (peerId) {
+          localStorageService.set('peerId', peerId);
+          $rootScope.$emit('peer-opened', peerId);
+          id = peerId;
+        });
+      }
+    };
+    /**
+     * This is a n async function that gives the  back the current id of the peer. It has to be async since
+     * the id could not be retrieved even once and in that case the callback is called after the id has been recieved.
+     * @param scope the current angular scope
+     * @param callback the callback function
+     */
+    this.getId = function (scope, callback) {
+      if (id!=null) {
+        callback(id);
+      } else {
+        var handler = $rootScope.$on('peer-opened', function(sender,id){
+          callback(id);
+        });
+        scope.$on('$destroy', handler);
+      }
+    };
+
+
+    /**
+     * Method to return the id of the other peer.
+     * @returns {string} the id of the other peer, if the connection has atleast been established once
+     */
+
+    this.getOtherId = function () {
+      return otherPeerId;
+    }
+
+    /**
+     * Internal method to get the object of the communicating service.
+     * @returns {object} peer object, so that the methods can be called.
+     */
+    var getObject = function () {
+      if (peer) {
+        return peer;
+      }
+      else {
+        return new Peer(id, {key: 'ytgklpf684u0udi'});
+      }
+    };
+
+    /**
+     * Internal function to start listening for connections. Emits an event to notify that the connection
+     * has been opened. The connection can then be accessed through the connection object of this class.
+     */
+    function openConnection() {
+      var peer = getObject();
+      peer.on('connection', function (connection) {
+        otherPeerId = connection.peer;
+        localStorageService.set('otherPeerId', otherPeerId);
+        connection.on('open', function () {
+          $rootScope.$emit('connection-open');
+          that.connection = connection;
+        });
+      });
+    }
+
+    /**
+     *
+     * @param scope {object} the current scope of the
+     * @param callback
+     */
+    this.subscribeConnectionOpen = function (scope, callback) {
+      var handler = $rootScope.$on('connection-open', callback);
+      scope.$on('$destroy', handler);
+      openConnection();
+    };
+
+    intialize();
+  };
+  return new Communications();
 });
